69bb0b53b54c5e4658822912a7ac8fdb714d4fd8
qoire <mr.sun.yao@gmail.com>
2018-07-20 15:27:04 -0400

    dev branch files

---
+++ pull_modules.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# looks for an external variable AION_ENV
+cp -rf ${AION_ENV}/modAion/modAion.iml .
+cp -rf ${AION_ENV}/modAionBase/modAionBase.iml .
+cp -rf ${AION_ENV}/modAionImpl/modAionImpl.iml .
+cp -rf ${AION_ENV}/modApiServer/modApiServer.iml .
+cp -rf ${AION_ENV}/modBoot/modBoot.iml .
+cp -rf ${AION_ENV}/modCrypto/modCrypto.iml .
+cp -rf ${AION_ENV}/modDbImpl/modDbImpl.iml .
+cp -rf ${AION_ENV}/modEvtMgr/modEvtMgr.iml .
+cp -rf ${AION_ENV}/modEvtMgrImpl/modEvtMgrImpl.iml .
+cp -rf ${AION_ENV}/modGui/modGui.iml .
+cp -rf ${AION_ENV}/modLogger/modLogger.iml .
+cp -rf ${AION_ENV}/modMcf/modMcf.iml .
+cp -rf ${AION_ENV}/modP2p/modP2p.iml .
+cp -rf ${AION_ENV}/modP2pImpl/modP2pImpl.iml .
+cp -rf ${AION_ENV}/modPrecompiled/modPrecompiled.iml .
+cp -rf ${AION_ENV}/modRlp/modRlp.iml .
+cp -rf ${AION_ENV}/modTxPool/modTxPool.iml .
+cp -rf ${AION_ENV}/modTxPoolImpl/modTxPoolImpl.iml .
+cp -rf ${AION_ENV}/modVM/modVM.iml .
+cp -rf ${AION_ENV}/aion_fastvm/modFastVM/modFastVM.iml .
